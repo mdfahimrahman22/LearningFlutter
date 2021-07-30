b0b45387363da430d1d778f3ca3cb81b2275df26
Fahim Rahman <57598021+fahimrahman002@users.noreply.github.com>
2021-07-31 00:34:16 +0600

    learned ScrollView, ListView, Container, MediaQuery

--- first_app/lib/main.dart
+++ first_app/lib/main.dart
@@ -3,28 +3,71 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MyApp());
 }
+
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+
+
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: SafeArea(
-        child: Scaffold(
-          appBar: AppBar(title: Text("Home Page"),
-          centerTitle: true,
-          leading: Icon(Icons.message),
-          actions:<Widget>[
-            IconButton(icon: Icon(Icons.add_a_photo),onPressed: (){},)
-          ]
-          ),
-          body: Center(
-            child: Text("This is my first app",
-            style:TextStyle(fontSize: 25,fontWeight: FontWeight.bold) ,),
-          ),
-        ),
-      ),
+      home: Homepage()
     );
   }
 }
+class Homepage extends StatelessWidget {
+  const Homepage({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    var height=MediaQuery.of(context).size.height;
+    var width=MediaQuery.of(context).size.width;
+    var sideMargin=3;
+    var topMargin=2;
+    return SafeArea(
+      child: Scaffold(
+          appBar: AppBar(
+              title: Text("Home Page"),
+              centerTitle: true,
+              actions: <Widget>[
+                IconButton(
+                  icon: Icon(Icons.add_a_photo),
+                  onPressed: () {},
+                )
+              ]),
+          body: ListView(
+              children:<Widget>[
+                Column(
+                  children: <Widget>[
+                    Container(
+                      height: height/topMargin,
+                      width: width/sideMargin,
+                      color: Colors.amber,
+                    ),
+                    SizedBox(
+                      height: 10,
+                      width: 10,
+                    ),
+                    Container(
+                      height: height/topMargin,
+                      width: width/sideMargin,
+                      color: Colors.blueAccent,
+                    ),
+                    SizedBox(
+                      height: 10,
+                      width: 10,
+                    ),
+                    Container(
+                      height: height/topMargin,
+                      width: width/sideMargin,
+                      color: Colors.blueAccent,
+                    )
+                  ],
+                ),
+              ]
+          )),
+    );
+  }
+}
+ 
