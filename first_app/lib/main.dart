816c102ea83f7fdcec39fa285c6542626cfa3033
Fahim Rahman <57598021+fahimrahman002@users.noreply.github.com>
2021-07-31 10:39:18 +0600

    listtile,stack,positioned, LiquidSwipe,Card

--- first_app/lib/main.dart
+++ first_app/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:liquid_swipe/liquid_swipe.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,65 +10,58 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-
-
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Homepage()
-    );
+    return MaterialApp(debugShowCheckedModeBanner: false, home: Homepage());
   }
 }
+
 class Homepage extends StatelessWidget {
   const Homepage({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    var height=MediaQuery.of(context).size.height;
-    var width=MediaQuery.of(context).size.width;
-    var sideMargin=3;
-    var topMargin=2;
+    var height = MediaQuery.of(context).size.height;
+    var width = MediaQuery.of(context).size.width;
+    var sideMargin = 3;
+    var topMargin = 2;
+    final pages = [
+      Card(
+        elevation: 10,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.only(bottomLeft:Radius.circular(50))
+        ),
+        child: Container(
+          height: 200,
+          width: 200,
+        ),
+        color: Colors.blueAccent,
+      ),
+      Container(
+        color: Colors.redAccent,
+      ),
+      Container(
+        color: Colors.blueAccent,
+      ),
+      Container(
+        color: Colors.amberAccent,
+      ),
+    ];
     return SafeArea(
-      child: Scaffold(
-          appBar: AppBar(
-              title: Text("Home Page"),
-              centerTitle: true,
-              actions: <Widget>[
-                IconButton(
-                  icon: Icon(Icons.add_a_photo),
-                  onPressed: () {},
-                )
-              ]),
-          body: ListView(
-              children:<Widget>[
-                Column(
-                  children: <Widget>[
-                    Container(
-                      height: height/topMargin,
-                      width: width/sideMargin,
-                      color: Colors.amber,
-                    ),
-                    SizedBox(
-                      height: 10,
-                      width: 10,
-                    ),
-                    Container(
-                      height: height/topMargin,
-                      width: width/sideMargin,
-                      color: Colors.blueAccent,
-                    ),
-                    SizedBox(
-                      height: 10,
-                      width: 10,
-                    ),
-                    Container(
-                      height: height/topMargin,
-                      width: width/sideMargin,
-                      color: Colors.blueAccent,
-                    )
-                  ],
-                ),
-              ]
-          )),
-    );
+        child: Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(Icons.add),
+      ),
+      appBar:
+          AppBar(title: Text("Home Page"), centerTitle: true, actions: <Widget>[
+        IconButton(
+          icon: Icon(Icons.add_a_photo),
+          onPressed: () {},
+        )
+      ]),
+      body: Center(
+          child: pages[0]
+
+      ),
+    ));
   }
 }
- 
